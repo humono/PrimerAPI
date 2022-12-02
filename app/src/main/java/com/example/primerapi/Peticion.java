c422e1622e88078b94bf65673d8de7add6c4308d
humono <j.glzbrt@gmail.com>
2022-12-02 17:29:34 +0100

    02/12/2022 Leemos JSon incluso con array

--- app/src/main/java/com/example/primerapi/Peticion.java
+++ app/src/main/java/com/example/primerapi/Peticion.java
@@ -37,10 +37,56 @@ public class Peticion extends Thread {
                 //          Voy leyendo el Json y recupero la clavve-valor y la imprimo en el Log.
                 while (jr.hasNext()) {
                     String clave = jr.nextName();
-                    String valor = jr.nextString();
-                    Log.v("elemento", clave + " - " + valor);
+                    //    String valor = jr.nextString();
+                    if (clave.equals("organization_url")) {
+                        String valor = jr.nextString();
+                        Log.v("elemento del Json", "CLAVE: " + clave + " - VALOR: " + valor);
+                    } else if (clave.equals("keys_url")) {
+                        String valor = jr.nextString();
+                        Log.v("elemento del Json", "CLAVE: " + clave + " - VALOR: " + valor);
+                    } else {
+                        jr.skipValue();
+                    }
                 }
                 jr.endObject();
+
+
+// https://www.zaragoza.es/sede/servicio/mascotas?rf=html&start=0&rows=50 VAMOS A LA URL DE LA API DE ZARAGOZA para probar un array
+                URL url2 = new URL("https://www.zaragoza.es/sede/servicio/mascotas?rf=html&start=0&rows=50");
+                HttpsURLConnection conn2 = (HttpsURLConnection) url2.openConnection();
+                conn2.setRequestProperty("Accept", "application/json");
+                if (conn2.getResponseCode() == 200) {
+                    InputStream is2 = conn2.getInputStream();
+                    InputStreamReader isr2 = new InputStreamReader(is2, "UTF-8");
+                    JsonReader jr2 = new JsonReader(isr2);
+                    jr2.beginObject();
+                    while (jr2.hasNext()) {
+                        String clave2 = jr2.nextName();
+                        String nombre = "", raza = "";
+                        if (clave2.equals("result")) {
+                            jr2.beginArray();
+                            while (jr2.hasNext()) {
+                                jr2.beginObject();
+                                while (jr2.hasNext()) {
+                                    String clave3 = jr2.nextName();
+                                    if (clave3.equals("nombre")) {
+                                        nombre = jr2.nextString();
+                                    } else if (clave3.equals("raza")) {
+                                        raza = jr2.nextString();
+                                    } else {
+                                        jr2.skipValue();
+                                    }
+                                    Log.v("Animal", "NOMBRE: " + nombre + " - RAZA: " + raza);
+                                }
+                                jr2.endObject();
+                            }
+                            jr2.endArray();
+                        } else {
+                            jr2.skipValue();
+                        }
+                    }
+                    jr2.endObject();
+                }
             }
         } catch (MalformedURLException mue) {
             mue.printStackTrace();
